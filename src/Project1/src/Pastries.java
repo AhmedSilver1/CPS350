43e463d78a6d00230cf61099cb793f48ea8afb50
AhmedSilver1 <163907539+AhmedSilver1@users.noreply.github.com>
2025-01-22 00:08:02 -0500

    Update Pastries.java

--- src/Project1/src/Pastries.java
+++ src/Project1/src/Pastries.java
@@ -1,4 +1,30 @@
 
-public class Pastries {
+public class Pastries extends CafeMenu {
+
+	//Fields
+	String pastrieName;
+	
+		//Constructor
+	Pastries(String pastrieName, String size, double price, int qunaitiy) {
+		super(size, price, qunaitiy);
+		this.pastrieName = pastrieName;
+	}
+
+	public String getPastrieName() {
+		return pastrieName;
+	}
+
+
+	public void setPastrieName(String pastrieName) {
+		this.pastrieName = pastrieName;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "Pastries [pastrieName=" + pastrieName + "]";
+	}
+
+	
 
 }
