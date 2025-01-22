e4050405e9d7c1bfbad6745a0c8c614c67ae62df
AhmedSilver1 <163907539+AhmedSilver1@users.noreply.github.com>
2025-01-22 00:07:43 -0500

    Update Tea.java

--- src/Project1/src/Tea.java
+++ src/Project1/src/Tea.java
@@ -1,4 +1,41 @@
 
-public class Tea {
+public class Tea  extends CafeMenu{
+	
+	
+	
+	//Fields
+	String teaName;
+	String teaType;
+	
+	//Constructor
+	Tea(String teaName, String teaType, String size, double price, String temp, int qunaitiy) {
+		super(size, price, temp, qunaitiy);
+		this.teaName = teaName;
+	}
 
+	
+	public String getTeaName() {
+		return teaName;
+	}
+
+	public void setTeaName(String teaName) {
+		this.teaName = teaName;
+	}
+
+	public String getTeaType() {
+		return teaType;
+	}
+
+	public void setTeaType(String teaType) {
+		this.teaType = teaType;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Tea [teaName=" + teaName + ", teaType=" + teaType + "]";
+	}
+
+	
+	
 }
