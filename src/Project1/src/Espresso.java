e7ae36fa1d72b429e1d346b17153948f26b623c7
AhmedSilver1 <163907539+AhmedSilver1@users.noreply.github.com>
2025-01-22 00:08:24 -0500

    Update Espresso.java

--- src/Project1/src/Espresso.java
+++ src/Project1/src/Espresso.java
@@ -1,14 +1,42 @@
 
 public class Espresso  extends CafeMenu{
 	
-
+	//Fields
 	private String esspressoName;
 	private int shots;
 	
-	
+	//Constructor
 	Espresso(String esspressoName,int shots,String size, double price, String temp, int qunaitiy) {
 		super(size, price, temp, qunaitiy);
-		this.esspressoName = esspressoName;
-		this.shots= shots;
+		this.setEsspressoName(esspressoName);
+		this.setShots(shots);
 	}
+
+
+
+
+	public String getEsspressoName() {
+		return esspressoName;
+	}
+
+
+	public void setEsspressoName(String esspressoName) {
+		this.esspressoName = esspressoName;
+	}
+
+
+	public int getShots() {
+		return shots;
+	}
+
+
+	public void setShots(int shots) {
+		this.shots = shots;
+	}	
+	
+	@Override
+	public String toString() {
+		return "Espresso [esspressoName=" + esspressoName + ", shots=" + shots + "]";
+	}
+
 }
